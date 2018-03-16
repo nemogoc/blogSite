d629650ec078f34c693ebfa3c5f2ff5b62ad1b6d
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-03-15 20:07:04 -0500

    add more project skeleton

--- app.js
+++ app.js
@@ -5,13 +5,23 @@ var express = require('express'),
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose.connect("mongodb://localhost/blogapp");
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/blogapp", function(err){
+  if(err){
+    console.log("Error connecting: " + err);
+  }
+});
 
 var postSchema = new mongoose.Schema({
   title: String,
-  img: String,
-  body: String
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now }
 });
 
 var Post = mongoose.model("Post", postSchema);
+
+
+app.listen(3000, function(){
+  console.log("serving on port 3000");
+});

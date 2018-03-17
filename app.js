bd354fb7ca21ffc0114c7d7d7a4b512cbe75a0ae
nemogoc <1668286+nemogoc@users.noreply.github.com>
2018-03-17 14:26:38 -0500

    add show route & page, unstyled. fix new form

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require('express'),
-    app = express(),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser');
+  app = express(),
+  mongoose = require('mongoose'),
+  bodyParser = require('body-parser');
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/blogapp", function(err){
-  if(err){
+mongoose.connect("mongodb://localhost/blogapp", function (err) {
+  if (err) {
     console.log("Error connecting: " + err);
   }
 });
@@ -16,47 +16,54 @@ var postSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
-  created: {type: Date, default: Date.now }
+  created: { type: Date, default: Date.now }
 });
 
 var Post = mongoose.model("Post", postSchema);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
   res.redirect("/posts");
 });
 
 //INDEX
-app.get("/posts", function(req, res){
-  Post.find({}, function(err, posts){
-    if(err){
+app.get("/posts", function (req, res) {
+  Post.find({}, function (err, posts) {
+    if (err) {
       console.log(err);
     }
-    else{
+    else {
       res.render("index", { posts: posts });
     }
   });
 });
 
 //NEW
-app.get("/posts/new", function(req,res){
+app.get("/posts/new", function (req, res) {
   res.render("new");
 });
 
 //CREATE
-app.post("/posts", function(req, res){
-  var createdDate;
-  if (req.body.post.created){
-    req.body.post.created = createdDate;
-  }
-
-  Post.create(req.body.post, function(err, post){
-    if (err){
+app.post("/posts", function (req, res) {
+  Post.create(req.body.post, function (err, post) {
+    if (err) {
       console.log(err);
     }
     res.redirect("/posts");
   });
 });
 
-app.listen(3000, function(){
+//SHOW
+app.get("/posts/:id", function (req, res) {
+  Post.findById(req.params.id, function (err, post) {
+    if (err) {
+      res.redirect("/posts");
+    }
+    else {
+      res.render("show", { post: post });
+    }
+  });
+});
+
+app.listen(3000, function () {
   console.log("serving on port 3000");
 });
